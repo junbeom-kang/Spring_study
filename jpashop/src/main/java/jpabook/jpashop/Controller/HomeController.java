f48d039e19615069c24d768fda8947fa7fabf766
junbeom-kang <kgood10369@gmail.com>
2021-03-22 23:57:54 +0900

    no message

--- jpashop/src/main/java/jpabook/jpashop/Controller/HomeController.java
+++ jpashop/src/main/java/jpabook/jpashop/Controller/HomeController.java
@@ -1,2 +1,16 @@
-package jpabook.jpashop.Controller;public class HomeController {
+package jpabook.jpashop.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        log.info("Home Controller");
+        return "home";
+    }
 }
--- jpashop/src/main/java/jpabook/jpashop/Controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/Controller/MemberController.java
@@ -1,2 +1,20 @@
-package jpabook.jpashop.Controller;public class MemberController {
+package jpabook.jpashop.Controller;
+
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "/members/createMemberForm";
+    }
 }
