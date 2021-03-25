3b3fedfcf78ca4b7345acbb9f44000d4bd73e419
junbeom-kang <kgood10369@gmail.com>
2021-03-25 22:12:59 +0900

    no message

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -12,12 +12,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberApiController {
     private final MemberService memberService;
 
-    @PostMapping("/api/v1/members")
-    public createMemberResponse saveMember(Member member) {
-        createMemberResponse
+    @PostMapping("/api/v1/members")// 엔티티를 바깥으로 들어내므로 안 좋은 방법임
+    public createMemberResponse saveMemberV1(Member member) {
+        Long memberId = memberService.join(member);
+        return new createMemberResponse(memberId);
+    }
+    @PostMapping("/api/v2/members")// 엔티티를 바깥으로 들어내므로 안 좋은 방법임
+    public createMemberResponse saveMemberV2(Request request) {
+        Member member = new Member();
+        member.setName(request.getName());
+        Long memberId = memberService.join(member);
+        return new createMemberResponse(memberId);
+
     }
     @Data
     static class createMemberResponse{
         private Long id;
+        public createMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Data
+    static class Request {
+        private String name;
     }
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,11 +18,14 @@ public class Order {
     @Id @GeneratedValue
     @Column(name="order_id")
     private Long id;
+
     @ManyToOne(fetch =FetchType.LAZY)
     @JoinColumn(name="member_id")  //FK를 member_id로잡아줌
     private Member member;
+
     @OneToMany(mappedBy = "order",cascade=CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
+
     @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
